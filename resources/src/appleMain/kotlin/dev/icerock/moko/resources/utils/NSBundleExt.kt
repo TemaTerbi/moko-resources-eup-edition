3c6c1e39bd0b2df824f6d47c59395150b381f156
Artem Solovev <avs149@europlan.ru>
2024-11-11 14:43:00 +0300

    feat: change NSBundle implementation

--- resources/src/appleMain/kotlin/dev/icerock/moko/resources/utils/NSBundleExt.kt
+++ resources/src/appleMain/kotlin/dev/icerock/moko/resources/utils/NSBundleExt.kt
@@ -2,7 +2,7 @@
  * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package dev.icerock.moko.resources.utils
+package dev.icerock.moko.resources.utils // must be same as in moko-resources to override method
 
 import platform.Foundation.NSBundle
 import platform.Foundation.NSDirectoryEnumerator
@@ -10,8 +10,15 @@ import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
 import platform.Foundation.pathExtension
 
+/**
+ * Workaround by MAX-POLKOVNIK from https://github.com/icerockdev/moko-resources/issues/747#issuecomment-2330854244
+ * Remove when https://github.com/icerockdev/moko-resources/issues/747 is resolved
+ */
+
+var nsBundle: NSBundle = NSBundle.mainBundle // <-- this is where we should looking for resources, by default mainBundle
+
 fun NSBundle.Companion.loadableBundle(identifier: String): NSBundle {
-    val bundlePath: String = NSBundle.mainBundle.bundlePath
+    val bundlePath: String = nsBundle.bundlePath // <-- path where we should search for bundle with resources
     val enumerator: NSDirectoryEnumerator = requireNotNull(NSFileManager.defaultManager.enumeratorAtPath(bundlePath))
     while (true) {
         val relativePath: String = enumerator.nextObject() as? String ?: break
